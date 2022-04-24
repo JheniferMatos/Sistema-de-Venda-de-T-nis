4341fba8bdfe72a5e4f18558c46b7d4723d3a625
RoberthOvelar <roberth.fontes@ufms.br>
2022-04-24 13:01:20 -0400

    Novo sql

--- sistema-de-cadastro-de-cliente/src/sql/comandos.sql
+++ sistema-de-cadastro-de-cliente/src/sql/comandos.sql
@@ -1,7 +1,12 @@
 CREATE TABLE `loja`.`cliente` (
   `idcliente` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `senha` VARCHAR(45) NOT NULL,
+  `cpf` VARCHAR(45) NOT NULL,
+  `cidade` VARCHAR(45) NOT NULL,
+  `estado` VARCHAR(45) NOT NULL,
+  `lagradouro` VARCHAR(45) NULL,
+  `numero` VARCHAR(45) NULL,
+  `cep` VARCHAR(45) NULL,
+  `telefone` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
   PRIMARY KEY (`idcliente`));
-
