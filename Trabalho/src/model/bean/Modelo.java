3d1f7c5e90b0dbd1b4241c528b28f513df1554cd
RoberthOvelar <roberth.fontes@ufms.br>
2022-05-24 10:06:38 -0400

    Modelos
    Designer view modelos

---
+++ Trabalho/src/model/bean/Modelo.java
@@ -0,0 +1,40 @@
+package model.bean;
+
+public class Modelo {
+    private int cod;
+    private String desc;
+    private int codMarca;
+    private float preco;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getCodMarca() {
+        return codMarca;
+    }
+
+    public void setCodMarca(int codMarca) {
+        this.codMarca = codMarca;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+}
