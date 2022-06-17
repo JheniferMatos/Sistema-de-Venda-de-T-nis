82ea28e696e9cf3a1f553de18061c2ae745d6bb7
lohanTT <104173036+lohanTT@users.noreply.github.com>
2022-06-17 14:21:13 -0400

    Alterações na controller

--- TrabalhoRevisado/src/controller/Controller.java
+++ TrabalhoRevisado/src/controller/Controller.java
@@ -1,37 +1,63 @@
 package controller;
 import java.util.ArrayList;
 import java.util.List;
-import model.bean.Devolucao;
+import model.bean.Marca;
 import model.bean.ModeloVendido;
 import model.bean.Venda;
-import model.dao.DevolucaoDAO;
+import model.bean.Devolucao;
+import model.dao.FuncionarioDAO;
+import model.dao.ClienteDAO;
+import model.dao.MarcaDAO;
+import model.dao.ModeloDAO;
 import model.dao.VendaDAO;
+import model.dao.DevolucaoDAO;
 import model.dao.ModeloVendidoDAO;
 
 public class Controller {
+
+    private FuncionarioDAO funcionarioDAO;
+    private ClienteDAO clienteDAO;
+    private MarcaDAO marcaDAO;
+    private ModeloDAO modeloDAO;
+    private ModeloVendidoDAO modeloVendidoDAO;
+    private VendaDAO vendaDAO;
+    private DevolucaoDAO devolucaoDAO;
+    
+    public Controller(){
+        this.funcionarioDAO = new FuncionarioDAO();
+        this.clienteDAO = new ClienteDAO();
+        this.marcaDAO = new MarcaDAO();
+        this.modeloDAO = new ModeloDAO();
+        this.modeloVendidoDAO = new ModeloVendidoDAO();        
+        this.vendaDAO = new VendaDAO();  
+        this.devolucaoDAO = new DevolucaoDAO();
+    }
+    
     public List<Venda> getVendas(){
-        VendaDAO vdao = new VendaDAO();
-        List<Venda> vendas = vdao.buscaVendas();
+        List<Venda> vendas = vendaDAO.buscaVendas();
         
         return vendas;
     }
     
-    public List<ModeloVendido> getModelosVendidos(int codVenda){   
-        ModeloVendidoDAO mvdao = new ModeloVendidoDAO();
-        List<ModeloVendido> modelosVendidos = mvdao.buscarModelosVendidos(codVenda);
+    public List<ModeloVendido> getModelosVendidos(int codVenda){
+        ArrayList<ModeloVendido> modelosVendidos = modeloVendidoDAO.buscarModelosVendidos(codVenda);
         
         return modelosVendidos;
     }
     
-    public void realizarDevolucao(int codModeloVendido, String motivo){
+    public List <Marca> getMarcas(){
+        List<Marca> marcas = marcaDAO.buscaMarcas();
+        
+        return marcas;
+    }
+	
+	    public void realizarDevolucao(int codModeloVendido, String motivo){
         //Declarando objetos necessários
         Devolucao devolucao = new Devolucao();
         ModeloVendido mVendido = new ModeloVendido();
-        DevolucaoDAO devDAO = new DevolucaoDAO();
-        ModeloVendidoDAO mvDAO = new ModeloVendidoDAO();
         
         //Criando modelo vendido
-        mVendido = mvDAO.buscaModeloVendido(codModeloVendido);
+        mVendido = modeloVendidoDAO.buscaModeloVendido(codModeloVendido);
         mVendido.setDevolvido(true);
         
         //Criando devolucao
@@ -39,7 +65,7 @@ public class Controller {
         devolucao.setMotivo(motivo);
         
         //Persistindo devolucao e atualizando estado do modeloVendido
-        devDAO.inserirDevolucao(devolucao);
-        mvDAO.alteraEstadoDevolvido(mVendido);
+        devolucaoDAO.inserirDevolucao(devolucao);
+        modeloVendidoDAO.alteraEstadoDevolvido(mVendido);
     }
 }
